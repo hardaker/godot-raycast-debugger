3be1b2b53523c65c92bd4e3ab12ec719400e24f1
Wes Hardaker <opensource@hardakers.net>
2025-07-07 06:19:25 -0700

    slight clean up

--- raycast_debugger.gd
+++ raycast_debugger.gd
@@ -11,9 +11,13 @@ var cast_lines: Array[Line2D] = []
 var hit_lines: Array[Line2D] = []
 var line_count: int = 0
 
+var last_from = null
+var last_to = null
+
 enum LineType { PROJECTION, HIT_LINE };
 
 func create_line(line_type: LineType = LineType.PROJECTION) -> Line2D:
+	"""Creates a Line2D object for displaying on the screen"""
 	# create our new line child
 	var line = Line2D.new()
 	line.global_position = Vector2(0, 0)
@@ -30,29 +34,19 @@ func create_line(line_type: LineType = LineType.PROJECTION) -> Line2D:
 	add_child(line)
 	return line	
 
-func _ready() -> void:
-	pass
-
 func _process(_delta: float) -> void:
+	"""Per-frame process routine that merely resets our line usage counter."""
 	line_count = 0
 
-func init() -> void:
-	# the_world_state = world_state
-	pass
-	
-var last_from = null
-var last_to = null
-
-func intersect_ray(from: Vector2, to: Vector2, exclude: Array = [], collide_with_areas: bool = true):
-	"""Tests a collision from a starting to ending vector"""
+func intersect_ray(
+		from: Vector2, to: Vector2, exclude: Array = [], collide_with_areas: bool = true
+		):
+	"""Test a collision from a starting to ending vector and memorize the ray."""
 
 	if line_count == 0:
 		for i in range(len(cast_lines)):
 			cast_lines[i].visible = false
 			hit_lines[i].visible = false
-
-	last_from = from
-	last_to = to
 
 	var world_state = get_world_2d().direct_space_state
 	var query = PhysicsRayQueryParameters2D.create(from, to)
@@ -80,12 +74,12 @@ func intersect_ray(from: Vector2, to: Vector2, exclude: Array = [], collide_with
 			
 		cast_line.visible = true
 		cast_line.global_position = Vector2(0, 0)
-		cast_line.points = PackedVector2Array([last_from, last_to])
+		cast_line.points = PackedVector2Array([from, last_to])
 
 		if result:
 			print("hit something: " + str(result.collider))
 			hit_line.global_position = Vector2(0, 0)
-			hit_line.points = PackedVector2Array([last_from, result.position])
+			hit_line.points = PackedVector2Array([from, result.position])
 			hit_line.visible = true
 		else:
 			# print("hit nothing")
